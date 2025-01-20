950aead7cbd74ea791b4af8bfab00282b140debb
TemurWebDev <temurbekqurbonov01tuit@gmail.com>
2025-01-21 03:09:12 +0500

    static

--- core/settings.py
+++ core/settings.py
@@ -144,16 +144,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#       (BASE_DIR/'static')
-#   ]
+STATICFILES_DIRS = [
+      (BASE_DIR/'static')
+  ]
 
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
+# STATIC_URL = "static/"
+# STATIC_ROOT = BASE_DIR / "static"
+# STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
 
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
